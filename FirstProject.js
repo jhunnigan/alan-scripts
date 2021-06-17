712b51f97b03f9955c3bacce2c3e8d9156b855d4
jhunnigan <jane.hunnigan@gmail.com>
2021-06-17 07:40:42 +0000

    test comit to public repo

---
+++ FirstProject.js
@@ -0,0 +1,11 @@
+// Use this sample to create your own voice commands
+intent('hello world', p=>  {
+    p.play('(hello|hi there)');
+});
+
+
+
+// Use this sample to create your own voice commands
+intent('hello Alan', p=>  {
+    p.play('Hello Jane');
+});
